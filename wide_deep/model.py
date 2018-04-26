3f6e2aaec0b765ff10123be70de56b5e4e70d2f3
jangmino <jangmin.o@gmail.com>
2018-04-26 14:48:09 +0900

    wide and deep - batch_normalization is modified correctly.

--- wide_deep/model.py
+++ wide_deep/model.py
@@ -132,7 +132,8 @@ class Model(object):
         )
 
     trainable_params = tf.trainable_variables()
-    self.opt = tf.train.GradientDescentOptimizer(learning_rate=self.lr)
+#    self.opt = tf.train.GradientDescentOptimizer(learning_rate=self.lr)
+    self.opt = tf.train.AdamOptimizer(self.lr)
     gradients = tf.gradients(self.loss, trainable_params)
     clip_gradients, _ = tf.clip_by_global_norm(gradients, 5)
 
